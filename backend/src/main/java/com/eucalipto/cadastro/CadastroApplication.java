6b3aa73943db6d9351b5bcce8015411b890daed5
Julimar <jadmjr@gmail.com>
2023-01-24 17:48:38 -0300

    mudei nome da classe principal

--- backend/src/main/java/com/eucalipto/cadastro/CadastroApplication.java
+++ backend/src/main/java/com/eucalipto/cadastro/CadastroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmovieApplication {
+public class CadastroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmovieApplication.class, args);
+		SpringApplication.run(CadastroApplication.class, args);
 	}
 
 }
