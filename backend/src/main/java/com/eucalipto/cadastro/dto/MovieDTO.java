e6e83a1f97081d81d3368ad0c00882fc735de979
Julimar <jadmjr@gmail.com>
2023-01-25 19:09:47 -0300

    code removal unused

--- backend/src/main/java/com/eucalipto/cadastro/dto/MovieDTO.java
+++ backend/src/main/java/com/eucalipto/cadastro/dto/MovieDTO.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,18 +19,6 @@ public class MovieDTO {
 	private Integer count;
 	private String image;
 
-	public MovieDTO() {
-
-	}
-
-	public MovieDTO(Long id, String title, Double score, Integer count, String image) {
-		this.id = id;
-		this.title = title;
-		this.score = score;
-		this.count = count;
-		this.image = image;
-	}
-	
 	public MovieDTO(Movie movie) {
 		id = movie.getId();
 		title = movie.getTitle();
@@ -40,54 +27,12 @@ public class MovieDTO {
 		image = movie.getImage();
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public Double getScore() {
-		return score;
-	}
-
-	public void setScore(Double score) {
-		this.score = score;
-	}
-
-	public Integer getCount() {
-		return count;
-	}
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-	
 	public void sacar() {
-		
+
 	}
+
 	public void sacar(String image) {
-		
+
 	}
-	
-	
-	
 
 }
