b2525a1c7b2dfb85dbb9549173e6e8f480ae6e39
Julimar <jadmjr@gmail.com>
2023-02-17 18:43:48 -0300

    using filters chain

--- backend/src/main/java/com/eucalipto/cadastro/controllers/MovieController.java
+++ backend/src/main/java/com/eucalipto/cadastro/controllers/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,21 +24,25 @@ public class MovieController {
 	@Autowired
 	private MovieService service;
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
 	@GetMapping
 	public Page<MovieDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
 	@GetMapping(value = "/{id}")
 	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<Movie> create(@RequestBody Movie movie) {
 		return service.saveMovie(movie);
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Movie> delete(@PathVariable Long id) {
 		return service.delete(id);
